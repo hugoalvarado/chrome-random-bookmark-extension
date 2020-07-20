dde1908cc3e883c82a71aa4ab8f5c1e2411404b0
Hugo Alvarado <hugo@britecore.com>
2020-07-19 19:32:57 -0600

    Remove popup and perform open action on main button click

--- backgroundOpenRandomBookmark.js
+++ backgroundOpenRandomBookmark.js
@@ -1,5 +1,26 @@
-  chrome.runtime.onInstalled.addListener(function() {
+chrome.runtime.onInstalled.addListener(function () {
+    //None
+});
 
 
+chrome.browserAction.onClicked.addListener(function (tab) {
+    chrome.bookmarks.getTree(function (results) {
+        let randomLeaf = getRandomLeaf(results[0]);
+        open(randomLeaf.url);
+    });
+});
 
-  });
+
+function getRandomInt(max) {
+    return Math.floor(Math.random() * Math.floor(max));
+}
+
+function getRandomLeaf(bookmarkTree) {
+    if (typeof (bookmarkTree.children) != 'undefined') {
+        let lenChildren = bookmarkTree.children.length
+        return getRandomLeaf(bookmarkTree.children[getRandomInt(lenChildren)])
+    } else if (typeof (bookmarkTree.title) != 'undefined') {
+        return bookmarkTree;
+    }
+    return false
+}
